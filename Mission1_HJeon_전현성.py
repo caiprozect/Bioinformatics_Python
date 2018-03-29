f9288ecd7d5167541e8cbe0545fa97dbdf423147
unknown <caiprozect@gmail.com>
2018-03-30 02:06:55 +0900

    Hotfix Done

--- Mission1_HJeon_전현성.py
+++ Mission1_HJeon_전현성.py
@@ -19,14 +19,14 @@ def make_seq(infile, outfile): #Sanity check and Make one line seq file
 	print("\t2. Seq file has been created")
 	print("\t=================================================")
 
-def chunk_process(file, start, size, polyLen):
+def chunk_process(file, start, size, length):
 	partFreqDict = defaultdict(int)
 	with open(file, "rb") as inFileH:
 		inFileH.seek(start)
 		sPartText = inFileH.read(size).upper()
 		nPartTextLen = len(sPartText)
-		for i in range(nPartTextLen-polyLen+1):
-			partFreqDict[sPartText[i:i+polyLen]] += 1
+		for i in range(nPartTextLen-length+1):
+			partFreqDict[sPartText[i:i+length]] += 1
 	return partFreqDict
 
 def make_chunk(file, size = 1024*1024*64):
